b2d510fb0e0b443c16f19cfef077bdf6a9c9f974
Douglas Tovar <douglastovar.g@gmail.com>
2019-09-09 17:42:00 -0400

    new header added

--- api/routes/Routes.js
+++ api/routes/Routes.js
@@ -4,6 +4,7 @@ const myRoutes = function (app) {
 
   app.all('/*', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     next();
   });
 
